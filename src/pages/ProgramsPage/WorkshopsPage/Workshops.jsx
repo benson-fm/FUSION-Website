e941c930ae17ef87b1da3d32e93900d3d2dd8236
Arvin Limjoco <ninjaghost041@gmail.com>
2024-08-11 12:53:56 -0700

    Initialized footer and navbar

---
+++ src/pages/ProgramsPage/WorkshopsPage/Workshops.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../../../components/Navbar"
+import Footer from "../../../components/Footer"
+import WorkshopsBackground from "../../../images/WorkshopsBackground.png"
+
+
+
+const Workshops = () => {
+    return(
+        <>
+        <Navbar title="WORKSHOPS" subtitle="PROGRAMS" bg={WorkshopsBackground}/>
+        <Footer></Footer>
+        </>
+
+    )
+}
+
+export default Workshops
