0fa3979c5a0df6ad868499f4733e66499c31772a
Arvin Limjoco <ninjaghost041@gmail.com>
2024-08-11 13:01:10 -0700

    Added JoinWorkshops file

--- src/pages/ProgramsPage/WorkshopsPage/Workshops.jsx
+++ src/pages/ProgramsPage/WorkshopsPage/Workshops.jsx
@@ -1,13 +1,14 @@
-import Navbar from "../../../components/Navbar"
-import Footer from "../../../components/Footer"
-import WorkshopsBackground from "../../../images/WorkshopsBackground.png"
-
+import Navbar from "../../../components/Navbar";
+import Footer from "../../../components/Footer";
+import WorkshopsBackground from "../../../images/WorkshopsBackground.png";
+import JoinWorkshops from "./JoinWorkshops.jsx";
 
 
 const Workshops = () => {
     return(
         <>
         <Navbar title="WORKSHOPS" subtitle="PROGRAMS" bg={WorkshopsBackground}/>
+        <JoinWorkshops></JoinWorkshops>
         <Footer></Footer>
         </>
 
