859029bc07edba1d3be92eab92fb467df15aa237
Arvin Limjoco <ninjaghost041@gmail.com>
2024-08-31 12:05:48 -0700

    Tried fixing background of Credits, I think defective due to how Navbar.jsx works

--- src/pages/CreditsPage/Credits.jsx
+++ src/pages/CreditsPage/Credits.jsx
@@ -3,11 +3,13 @@ import Footer from "../../components/Footer";
 import OurFusiteTeam from "./OurFusiteTeam";
 import Designers from "./Designers";
 import Developers from "./Developers";
+import CreditsBackground from "../../images/CreditsBackground.jpg";
+
 const Credits = () => {
     return (
         <>
             <div className="overflow-x-hidden">
-                <Navbar title="FUSITE TEAM" subtitle="Credits"/>
+                <Navbar bg={CreditsBackground} title="FUSITE TEAM" subtitle="Credits"/>
                 <OurFusiteTeam></OurFusiteTeam>
                 <Designers> </Designers>
                 <Developers></Developers>
