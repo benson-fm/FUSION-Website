8a30922c590a253578b0d651a8cac1b10c04729d
Benson Manzano <bfmanzan@uci.edu>
2024-09-04 22:01:40 -0700

    Added links and pages for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,43 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import Family from "./pages/Family";
+import Internship from "./pages/Internship";
+import AboutUs from "./pages/AboutUs";
+import Home from "./pages/HomePage/Home";
+import Programs from "./pages/ProgramsPage/programs";
+import { MeetingsPage } from "./pages/meetingsPage";
+import Social from "./pages/Social";
+import Board from "./pages/Board";
+import { AlumniPage } from "./pages/AlumniPage";
+import { MentorshipPage } from "./pages/MentorshipPage";
+import Projects from "./pages/ProjectsPage/Projects";
+import Fundraising from "./pages/ProgramsPage/FundraisingPage/Fundraising";
+import StudyHours from "./pages/StudyHours";
+import Workshops from "./pages/ProgramsPage/WorkshopsPage/Workshops.jsx";
+import FuFit from "./pages/FuFit";
 
-import Family from './pages/Family'
-import Internship from './pages/Internship'
-import AboutUs from './pages/AboutUs'
-import Home from './pages/HomePage/Home'
-import Programs from './pages/ProgramsPage/programs'
-import { MeetingsPage } from './pages/meetingsPage'
-import Social from './pages/Social'
-import Board from './pages/Board'
-import { AlumniPage } from './pages/AlumniPage'
-import { MentorshipPage } from './pages/MentorshipPage'
-import Projects from './pages/ProjectsPage/Projects'
-import Fundraising from './pages/ProgramsPage/FundraisingPage/Fundraising'
-import StudyHours from './pages/StudyHours'
-import Workshops from './pages/ProgramsPage/WorkshopsPage/Workshops.jsx'
-import FuFit from './pages/FuFit'
-
-import Credits from './pages/CreditsPage/Credits'
+import Credits from "./pages/CreditsPage/Credits";
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/Board" element={<Board />} />
         <Route path="/" element={<Home />} />
-        <Route path="/Family" element={<Family />} />
-        <Route path="/Internship" element={<Internship />} />
-        <Route path="/programs/studyhours" element={<StudyHours />} />
-        <Route path="/programs" element={<Programs/>} />
-        <Route path="/AboutUs" element={<AboutUs />} />
-        <Route path="/meetingsPage" element={<MeetingsPage />} />
-        <Route path="/Social" element={<Social />} />
-        <Route path="/alumni" element={<AlumniPage />} />
-        <Route path="/mentorship" element={<MentorshipPage />} />
-        <Route path="/Projects" element={<Projects/>} />
-        <Route path="/programs/fundraising" element={<Fundraising/>}/>
-        <Route path="/programs/workshops" element={<Workshops/>}/>
-        <Route path="/FuFit" element={<FuFit/>} />
-        <Route path="/Credits" element={<Credits/>}/>
+        <Route path="/board" element={<Board />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/meetings" element={<MeetingsPage />} />
+        <Route path="/programs" element={<Programs />} />
+        <Route path="/programs/social" element={<Social />} />
+        <Route path="/programs/alumni" element={<AlumniPage />} />
+        <Route path="/programs/mentorship" element={<MentorshipPage />} />
+        <Route path="/programs/family" element={<Family />} />
+        <Route path="/programs/internship" element={<Internship />} />
+        <Route path="/programs/studyHours" element={<StudyHours />} />
+        <Route path="/programs/fundraising" element={<Fundraising />} />
+        <Route path="/programs/workshops" element={<Workshops />} />
+        <Route path="/programs/fufit" element={<FuFit />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/credits" element={<Credits />} />
       </Routes>
     </Router>
-  )
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 function renderTitle(title, subtitle, homeNav) {
   if (homeNav) {
     return (
-      <div className="relative justify-center content-center text-center justify-items-center space-y-12 p-24 z-50 flex-1 tracking-wide">
+      <div className="relative justify-center content-center text-center justify-items-center space-y-12 p-24 z-40 flex-1 tracking-wide">
         <div className="text-3xl font-light">EST. 2001</div>
         <div className="text-8xl font-light">
           WELCOME TO <br />
@@ -19,7 +19,7 @@ function renderTitle(title, subtitle, homeNav) {
     );
   } else {
     return (
-      <div className="relative justify-center content-center text-center justify-items-center p-24 z-50 flex-1 tracking-wide font-karla">
+      <div className="relative justify-center content-center text-center justify-items-center p-24 z-40 flex-1 tracking-wide font-karla">
         <div className="text-3xl">{subtitle}</div>
         <div className="text-9xl font-extrabold">{title}</div>
       </div>
@@ -34,37 +34,75 @@ function Navbar({ bg, title, subtitle, homeNav = false }) {
     navigate("/");
   };
   const handleAbout = () => {
-    navigate("/AboutUs");
+    navigate("/aboutUs");
   };
   const handleProjects = () => {
-    navigate("/Projects");
+    navigate("/projects");
   };
   const handleBoard = () => {
-    navigate("/Board");
+    navigate("/board");
   };
   const handlePrograms = () => {
     navigate("/programs");
   };
-  const handleFusionCon = () => {
-    navigate("/fusioncon");
+  const handleMeetings = () => {
+    navigate("/meetings");
   };
 
   return (
     <>
       <div className="absolute top-0 left-0 right-0 bottom-0 z-10 bg-zinc-800 opacity-60 h-dvh"></div>
       <div
-        className="bg-cover p-12 text-white h-dvh flex flex-col"
+        className="bg-cover p-16 text-white h-dvh flex flex-col"
         style={{ backgroundImage: `url(${bg}) ` }}
       >
-        <div className="relative flex flex-row justify-between z-50 ">
-          <img className="justify-self-start" src={FUSION} width={200} />
-          <div className="flex justify-self-end space-x-16 text-xl">
+        <div className="relative flex flex-row justify-between z-50 items-center">
+          <img className="justify-self-start h-auto w-56" src={FUSION} />
+          <div className="flex justify-self-end space-x-10 2xl:space-x-12 text-xl tracking-widest font-karla">
             <button onClick={handleHome}>HOME</button>
             <button onClick={handleAbout}>ABOUT</button>
-            <button onClick={handleProjects}>PROJECTS</button>
+            <button onClick={handleMeetings}>MEETINGS</button>
             <button onClick={handleBoard}>BOARD</button>
-            <button onClick={handlePrograms}>PROGRAMS</button>
-            <button onClick={handleFusionCon}>FUSIONCON</button>
+            {/* <button onClick={handlePrograms}>PROGRAMS</button> */}
+            <div className="dropdown dropdown-hover">
+              <div tabIndex={0} role="button" className="m-1" onClick={handlePrograms}>
+                PROGRAMS
+              </div>
+              <ul
+                tabIndex={0}
+                className="dropdown-content menu bg-[#0F0A0080] rounded-box z-[1] w-52 p-2 shadow"
+              >
+
+                <li>
+                  <a href="/programs/internship">INTERNSHIP</a>
+                </li>
+                <li>
+                  <a href="/programs/mentorship">MENTORSHIP</a>
+                </li>
+                <li>
+                  <a href="/programs/family">FAMILY</a>
+                </li>
+                <li>
+                  <a href="/programs/fundraising">FUNDRAISING</a>
+                </li>
+                <li>
+                  <a href="/programs/alumni">ALUMNI</a>
+                </li>
+                <li>
+                  <a href="/programs/workshops">WORKSHOPS</a>
+                </li>
+                <li>
+                  <a href="/programs/fufit">FUSION FIT</a>
+                </li>
+                <li>
+                  <a href="/programs/studyHours">STUDY HOURS</a>
+                </li>
+                <li>
+                  <a href="/programs/social">SOCIAL</a>
+                </li>
+              </ul>
+            </div>
+            <button onClick={handleProjects}>PROJECTS</button>
           </div>
         </div>
         {renderTitle(title, subtitle, homeNav)}
